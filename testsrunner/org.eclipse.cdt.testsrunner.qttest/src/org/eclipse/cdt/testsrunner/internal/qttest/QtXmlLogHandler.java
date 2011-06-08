b366e3923d4f3ca2f03fda5365f0a07c23f4d488
xgsa <xgsa@yandex.ru>
2011-06-08 15:08:46 +0300

    Qt Tests Runner was implemented

--- testsrunner/org.eclipse.cdt.testsrunner.qttest/src/org/eclipse/cdt/testsrunner/internal/qttest/QtXmlLogHandler.java
+++ testsrunner/org.eclipse.cdt.testsrunner.qttest/src/org/eclipse/cdt/testsrunner/internal/qttest/QtXmlLogHandler.java
@@ -5,9 +5,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.cdt.testsrunner.model.IModelManager;
-import org.eclipse.cdt.testsrunner.model.ITestItem;
+import org.eclipse.cdt.testsrunner.model.ITestCase;
 import org.eclipse.cdt.testsrunner.model.ITestMessage;
-import org.eclipse.cdt.testsrunner.model.ITestItem.Status;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
@@ -16,43 +15,85 @@ import org.xml.sax.helpers.DefaultHandler;
 
 public class QtXmlLogHandler extends DefaultHandler {
 	
-	private static final String XML_NODE_TEST_LOG = "TestLog"; //$NON-NLS-1$
-	private static final String XML_NODE_TEST_SUITE = "TestSuite"; //$NON-NLS-1$
 	private static final String XML_NODE_TEST_CASE = "TestCase"; //$NON-NLS-1$
-	private static final String XML_NODE_TESTING_TIME = "TestingTime"; //$NON-NLS-1$
-	private static final String XML_NODE_LAST_CHECKPOINT = "LastCheckpoint"; //$NON-NLS-1$
-	
-	
-	private static final String XML_NODE_INFO = "Info"; //$NON-NLS-1$
+	private static final String XML_NODE_TEST_FUNCTION = "TestFunction"; //$NON-NLS-1$
+	private static final String XML_NODE_INCIDENT = "Incident"; //$NON-NLS-1$
 	private static final String XML_NODE_MESSAGE = "Message"; //$NON-NLS-1$
-	private static final String XML_NODE_WARNING = "Warning"; //$NON-NLS-1$
-	private static final String XML_NODE_ERROR = "Error"; //$NON-NLS-1$
-	private static final String XML_NODE_FATAL_ERROR = "FatalError"; //$NON-NLS-1$
-	private static final String XML_NODE_EXCEPTION = "Exception"; //$NON-NLS-1$
+	private static final String XML_NODE_DESCRIPTION = "Description"; //$NON-NLS-1$
+
+	private static final String XML_NODE_ENVIRONMENT = "Environment"; //$NON-NLS-1$
+	private static final String XML_NODE_QTVERSION = "QtVersion"; //$NON-NLS-1$
+	private static final String XML_NODE_QTESTVERSION = "QTestVersion"; //$NON-NLS-1$
+	private static final String XML_NODE_BENCHMARK = "BenchmarkResult"; //$NON-NLS-1$
+	private static final String XML_NODE_DATATAG = "DataTag"; //$NON-NLS-1$
 	
-	
-	private static final String XML_ATTR_TEST_SUITE_NAME = "name"; //$NON-NLS-1$
+	private static final String XML_VALUE_INCIDENT_PASS = "pass"; //$NON-NLS-1$
+	private static final String XML_VALUE_INCIDENT_XFAIL = "xfail"; //$NON-NLS-1$
+	private static final String XML_VALUE_INCIDENT_FAIL = "fail"; //$NON-NLS-1$
+	private static final String XML_VALUE_INCIDENT_XPASS = "xpass"; //$NON-NLS-1$
+	private static final String XML_VALUE_INCIDENT_UNKNOWN = "??????"; //$NON-NLS-1$
+
+	private static final String XML_VALUE_MESSAGE_WARN = "warn"; //$NON-NLS-1$
+	private static final String XML_VALUE_MESSAGE_SYSTEM = "system"; //$NON-NLS-1$
+	private static final String XML_VALUE_MESSAGE_QDEBUG = "qdebug"; //$NON-NLS-1$
+	private static final String XML_VALUE_MESSAGE_QWARN = "qwarn"; //$NON-NLS-1$
+	private static final String XML_VALUE_MESSAGE_QFATAL = "qfatal"; //$NON-NLS-1$
+	private static final String XML_VALUE_MESSAGE_SKIP = "skip"; //$NON-NLS-1$
+	private static final String XML_VALUE_MESSAGE_INFO = "info"; //$NON-NLS-1$
+	private static final String XML_VALUE_MESSAGE_UNKNOWN = "??????"; //$NON-NLS-1$
+
 	private static final String XML_ATTR_TEST_CASE_NAME = "name"; //$NON-NLS-1$
-	private static final String XML_ATTR_MESSAGE_FILE = "file"; //$NON-NLS-1$
-	private static final String XML_ATTR_MESSAGE_LINE = "line"; //$NON-NLS-1$
+	private static final String XML_ATTR_TEST_FUNCTION_NAME = "name"; //$NON-NLS-1$
+	private static final String XML_ATTR_TYPE = "type"; //$NON-NLS-1$
+	private static final String XML_ATTR_FILE = "file"; //$NON-NLS-1$
+	private static final String XML_ATTR_LINE = "line"; //$NON-NLS-1$
 	
     private static final Map<String, ITestMessage.Level> STRING_TO_MESSAGE_LEVEL;
     static {
         Map<String, ITestMessage.Level> aMap = new HashMap<String, ITestMessage.Level>();
-        aMap.put(XML_NODE_INFO, ITestMessage.Level.Info);
-        aMap.put(XML_NODE_MESSAGE, ITestMessage.Level.Message);
-        aMap.put(XML_NODE_WARNING, ITestMessage.Level.Warning);
-        aMap.put(XML_NODE_ERROR, ITestMessage.Level.Error);
-        aMap.put(XML_NODE_FATAL_ERROR, ITestMessage.Level.FatalError);
+        aMap.put(XML_VALUE_MESSAGE_WARN, ITestMessage.Level.Warning);
+        aMap.put(XML_VALUE_MESSAGE_SYSTEM, ITestMessage.Level.Message);
+        aMap.put(XML_VALUE_MESSAGE_QDEBUG, ITestMessage.Level.Info);
+        aMap.put(XML_VALUE_MESSAGE_QWARN, ITestMessage.Level.Warning);
+        aMap.put(XML_VALUE_MESSAGE_QFATAL, ITestMessage.Level.FatalError);
+        aMap.put(XML_VALUE_MESSAGE_SKIP, ITestMessage.Level.Info);
+        aMap.put(XML_VALUE_MESSAGE_INFO, ITestMessage.Level.Info);
+        aMap.put(XML_VALUE_MESSAGE_UNKNOWN, ITestMessage.Level.FatalError);
         // NOTE: Exception node is processed separately
         STRING_TO_MESSAGE_LEVEL = Collections.unmodifiableMap(aMap);
     }
 
+    private static final Map<String, ITestCase.Status> STRING_TO_TEST_STATUS;
+    static {
+        Map<String, ITestCase.Status> aMap = new HashMap<String, ITestCase.Status>();
+        aMap.put(XML_VALUE_INCIDENT_PASS, ITestCase.Status.Passed);
+        aMap.put(XML_VALUE_INCIDENT_XFAIL, ITestCase.Status.Aborted);
+        aMap.put(XML_VALUE_INCIDENT_FAIL, ITestCase.Status.Aborted);
+        aMap.put(XML_VALUE_INCIDENT_XPASS, ITestCase.Status.Passed);
+        aMap.put(XML_VALUE_INCIDENT_UNKNOWN, ITestCase.Status.Aborted);
+        // NOTE: Exception node is processed separately
+        STRING_TO_TEST_STATUS = Collections.unmodifiableMap(aMap);
+    }
+
+    private static final Map<String, ITestMessage.Level> STRING_INCIDENT_TO_MESSAGE_LEVEL;
+    static {
+        Map<String, ITestMessage.Level> aMap = new HashMap<String, ITestMessage.Level>();
+        aMap.put(XML_VALUE_INCIDENT_PASS, ITestMessage.Level.Info);
+        aMap.put(XML_VALUE_INCIDENT_XFAIL, ITestMessage.Level.FatalError);
+        aMap.put(XML_VALUE_INCIDENT_FAIL, ITestMessage.Level.FatalError);
+        aMap.put(XML_VALUE_INCIDENT_XPASS, ITestMessage.Level.Info);
+        aMap.put(XML_VALUE_INCIDENT_UNKNOWN, ITestMessage.Level.FatalError);
+        // NOTE: Exception node is processed separately
+        STRING_INCIDENT_TO_MESSAGE_LEVEL = Collections.unmodifiableMap(aMap);
+    }
+
 	private IModelManager modelManager;
 	private String elementData;
+	private String messageText;
 	private String fileName;
 	private int lineNumber;
-	private ITestItem.Status testStatus;
+	private ITestMessage.Level messageLevel;
+	private long testStartTime;
 	
 	QtXmlLogHandler(IModelManager modelBuilder) {
 		this.modelManager = modelBuilder;
@@ -60,77 +101,70 @@ public class QtXmlLogHandler extends DefaultHandler {
 	
 	public void startElement(String namespaceURI, String localName, String qName, Attributes attrs) throws SAXException {
 		
-		if (qName == XML_NODE_TEST_SUITE) {
-			String testSuiteName = attrs.getValue(XML_ATTR_TEST_SUITE_NAME);
+		elementData = null;
+		if (qName == XML_NODE_TEST_CASE) {
+			// NOTE: Terminology mapping: Qt Test Case is actually a Test Suite
+			String testSuiteName = attrs.getValue(XML_ATTR_TEST_CASE_NAME);
 			modelManager.enterTestSuite(testSuiteName);
 
-		} else if (qName == XML_NODE_TEST_CASE) {
-			String testCaseName = attrs.getValue(XML_ATTR_TEST_CASE_NAME);
+		} else if (qName == XML_NODE_TEST_FUNCTION) {
+			// NOTE: Terminology mapping: Qt Test Function is actually a Test Case
+			String testCaseName = attrs.getValue(XML_ATTR_TEST_FUNCTION_NAME);
 			modelManager.enterTestCase(testCaseName);
-			testStatus = Status.Passed;
+			testStartTime = System.currentTimeMillis();
 
-		} else if (STRING_TO_MESSAGE_LEVEL.containsKey(qName)
-				|| qName == XML_NODE_LAST_CHECKPOINT) {
-			fileName = attrs.getValue(XML_ATTR_MESSAGE_FILE);
-			lineNumber = Integer.parseInt(attrs.getValue(XML_ATTR_MESSAGE_LINE).trim());
-			
-		} else if (qName == XML_NODE_EXCEPTION) {
-			elementData = null;
-			fileName = null;
-			lineNumber = -1;
-			
-		} else if (qName == XML_NODE_TESTING_TIME || qName == XML_NODE_TEST_LOG) {
+		} else if (qName == XML_NODE_MESSAGE) {
+			fileName = attrs.getValue(XML_ATTR_FILE);
+			lineNumber = Integer.parseInt(attrs.getValue(XML_ATTR_LINE).trim());
+			messageLevel = STRING_TO_MESSAGE_LEVEL.get(attrs.getValue(XML_ATTR_TYPE));			
+			messageText = null;
+
+		} else if (qName == XML_NODE_INCIDENT) {
+			fileName = attrs.getValue(XML_ATTR_FILE);
+			lineNumber = Integer.parseInt(attrs.getValue(XML_ATTR_LINE).trim());
+			String strType = attrs.getValue(XML_ATTR_TYPE);
+			messageLevel = STRING_INCIDENT_TO_MESSAGE_LEVEL.get(strType);
+			modelManager.setTestStatus(STRING_TO_TEST_STATUS.get(strType));
+			messageText = null;
+
+		} else if (qName == XML_NODE_DESCRIPTION
+				|| qName == XML_NODE_ENVIRONMENT
+				|| qName == XML_NODE_QTVERSION
+				|| qName == XML_NODE_QTESTVERSION
+				|| qName == XML_NODE_BENCHMARK
+				|| qName == XML_NODE_DATATAG) {
 			/* just skip, do nothing */
-			
+
 		} else {
 			String message = "Invalid XML format: Element \""+qName+"\" is not accepted!";
 			Activator.logErrorMessage(message);
 			throw new SAXException(message);
-		}
-	}
-	
-	private void addCurrentMessage(ITestMessage.Level level) throws SAXException {
-		if (elementData == null) {
-			String message = "Invalid XML format: Empty message text is not accepted!";
-			Activator.logErrorMessage(message);
-			throw new SAXException(message);
-		}
-		modelManager.addTestMessage(fileName, lineNumber, level, elementData);
-		elementData = null;
-		fileName = null;
-		lineNumber = -1;
-		if (level == ITestMessage.Level.Error) {
-			if (testStatus != ITestItem.Status.Aborted) {
-				testStatus = ITestItem.Status.Failed;
-			}
-			
-		} else if (level == ITestMessage.Level.FatalError || level == ITestMessage.Level.Exception) {
-			testStatus = ITestItem.Status.Aborted;
 		}
 	}
 
 	public void endElement(String namespaceURI, String localName, String qName) throws SAXException {
 
-		if (qName == XML_NODE_TEST_SUITE) {
+		if (qName == XML_NODE_TEST_CASE) {
 			modelManager.exitTestSuite();
 
-		} else if (qName == XML_NODE_TEST_CASE) {
-			modelManager.setTestStatus(testStatus);
+		} else if (qName == XML_NODE_TEST_FUNCTION) {
+			modelManager.setTestingTime((int)(System.currentTimeMillis()-testStartTime));
+			testStartTime = 0;
 			modelManager.exitTestCase();
 		
-		} else if (qName == XML_NODE_TESTING_TIME) {
-			modelManager.setTestingTime(Integer.parseInt(elementData.trim()));
-
-		} else if (STRING_TO_MESSAGE_LEVEL.containsKey(qName)) {
-			addCurrentMessage(STRING_TO_MESSAGE_LEVEL.get(qName));
-
-		} else if (qName == XML_NODE_EXCEPTION) {
-			if (fileName != null && lineNumber != -1) {
-				elementData += "\nSee the last checkpoint attached.";
+		} else if (qName == XML_NODE_MESSAGE || qName == XML_NODE_INCIDENT) {
+			if (messageText != null) {
+				modelManager.addTestMessage(fileName, lineNumber, messageLevel, messageText);
 			}
-			addCurrentMessage(STRING_TO_MESSAGE_LEVEL.get(qName));
 
-		} else if (qName == XML_NODE_TEST_LOG || qName == XML_NODE_LAST_CHECKPOINT) {
+		} else if (qName == XML_NODE_DESCRIPTION) {
+			messageText = elementData.isEmpty() ? null : elementData;
+
+		} else if (qName == XML_NODE_ENVIRONMENT
+				|| qName == XML_NODE_QTVERSION
+				|| qName == XML_NODE_QTESTVERSION
+				|| qName == XML_NODE_BENCHMARK
+				|| qName == XML_NODE_DATATAG) {
 			/* just skip, do nothing */
 			
 		} else {
@@ -138,6 +172,7 @@ public class QtXmlLogHandler extends DefaultHandler {
 			Activator.logErrorMessage(message);
 			throw new SAXException(message);
 		}
+		elementData = null;
 	}
 
 	public void characters(char[] ch, int start, int length) {
