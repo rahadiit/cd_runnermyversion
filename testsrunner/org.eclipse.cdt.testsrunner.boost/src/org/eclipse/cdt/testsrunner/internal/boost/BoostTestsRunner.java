b6d304d13d4ba2a724530ccb1e05b59b584eed14
xgsa <xgsa@yandex.ru>
2011-06-08 14:13:50 +0300

    Boost Tests Runner was implemented

--- testsrunner/org.eclipse.cdt.testsrunner.boost/src/org/eclipse/cdt/testsrunner/internal/boost/BoostTestsRunner.java
+++ testsrunner/org.eclipse.cdt.testsrunner.boost/src/org/eclipse/cdt/testsrunner/internal/boost/BoostTestsRunner.java
@@ -1,15 +1,33 @@
 package org.eclipse.cdt.testsrunner.internal.boost;
 
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
 import org.eclipse.cdt.testsrunner.launcher.ITestsRunner;
+import org.eclipse.cdt.testsrunner.model.IModelBuilder;
+import org.xml.sax.SAXException;
 
 public class BoostTestsRunner implements ITestsRunner {
 
-	public BoostTestsRunner() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public String run(String test) {
-		return "Yahooo!!!";
+	public void run(IModelBuilder modelBuilder) {
+		try {
+			SAXParserFactory spf = SAXParserFactory.newInstance();
+			SAXParser sp = spf.newSAXParser();
+			sp.parse("/tmp/boost_log.xml", new BoostXmlLogHandler(modelBuilder));
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
