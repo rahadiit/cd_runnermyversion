d8d41f7f1a881747c9ddedabe41e4de606fe3cf4
xgsa <xgsa@yandex.ru>
2011-06-08 15:11:22 +0300

    messages view was improved

--- testsrunner/org.eclipse.cdt.testsrunner/src/org/eclipse/cdt/testsrunner/internal/ui/view/MessagesPanel.java
+++ testsrunner/org.eclipse.cdt.testsrunner/src/org/eclipse/cdt/testsrunner/internal/ui/view/MessagesPanel.java
@@ -10,12 +10,14 @@
  *******************************************************************************/
 package org.eclipse.cdt.testsrunner.internal.ui.view;
 
-import java.util.HashSet;
+import java.util.Comparator;
 import java.util.Set;
+import java.util.TreeSet;
 
 import org.eclipse.cdt.testsrunner.model.IModelVisitor;
 import org.eclipse.cdt.testsrunner.model.ITestCase;
 import org.eclipse.cdt.testsrunner.model.ITestItem;
+import org.eclipse.cdt.testsrunner.model.ITestLocation;
 import org.eclipse.cdt.testsrunner.model.ITestMessage;
 import org.eclipse.cdt.testsrunner.model.ITestSuite;
 import org.eclipse.jface.viewers.IStructuredContentProvider;
@@ -73,7 +75,48 @@ public class MessagesPanel {
 		}
 
 		private void collectMessages(ITestItem[] testItems) {
-			Set<ITestMessage> testMessagesSet = new HashSet<ITestMessage>();
+			Set<ITestMessage> testMessagesSet = new TreeSet<ITestMessage>(new Comparator<ITestMessage>() {
+
+				public int compare(ITestMessage message1, ITestMessage message2) {
+					// Compare messages by location
+					ITestLocation location1 = message1.getLocation();
+					ITestLocation location2 = message2.getLocation();
+					
+					if (location1 != null && location2 != null) {
+						// Compare by file name
+						String file1 = location1.getFile();
+						String file2 = location2.getFile();
+						int fileResult = file1.compareTo(file2);
+						if (fileResult != 0) {
+							return fileResult;
+						} else {
+							// Compare by line number
+							int line1 = location1.getLine();
+							int line2 = location2.getLine();
+							if (line1 < line2) {
+								return -1;
+								
+							} else if (line1 > line2) {
+								return 1;
+								
+							} else {
+								// Compare by message text
+								String text1 = message1.getText();
+								String text2 = message2.getText();
+								return text1.compareTo(text2);
+							}
+						}
+						
+					} else if (location1 == null && location2 != null) {
+						return -1;
+						
+					} else if (location1 != null && location2 == null) {
+						return 1;
+					}
+					
+					return 0;
+				}
+			});
 			for (ITestItem testItem : testItems) {
 				testItem.visit(new MessagesCollector(testMessagesSet));
 			}
@@ -83,7 +126,38 @@ public class MessagesPanel {
 
 	class MessagesLabelProvider extends LabelProvider implements ITableLabelProvider {
 		public String getColumnText(Object obj, int index) {
-			return ((ITestMessage)obj).getText();
+			StringBuilder sb = new StringBuilder();
+			ITestMessage message = (ITestMessage)obj;
+			ITestLocation location = message.getLocation();
+			if (location != null) {
+				sb.append(location.getFile());
+				sb.append("(");
+				sb.append(location.getLine());
+				sb.append("): ");
+			}
+			switch (message.getLevel()) {
+				case Info:
+					sb.append("Info");
+					break;
+				case Message:
+					sb.append("Message");
+					break;
+				case Warning:
+					sb.append("Warning");
+					break;
+				case Error:
+					sb.append("Error");
+					break;
+				case FatalError:
+					sb.append("Fatal error");
+					break;
+				case Exception:			
+					sb.append("Exception");
+					break;
+			}
+			sb.append(": ");
+			sb.append(message.getText());
+			return sb.toString();
 		}
 		public Image getColumnImage(Object obj, int index) {
 			return getImage(obj);
