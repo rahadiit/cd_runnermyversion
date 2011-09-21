1d19eb456bfce3e710699689754be641b9d37e51
xgsa <xgsa@yandex.ru>
2011-09-21 09:40:55 +0300

    Unit tests for Google Tests Runner were implemented

--- testsrunner/org.eclipse.cdt.testsrunner.gtest/src/org/eclipse/cdt/testsrunner/internal/gtest/OutputHandler.java
+++ testsrunner/org.eclipse.cdt.testsrunner.gtest/src/org/eclipse/cdt/testsrunner/internal/gtest/OutputHandler.java
@@ -32,10 +32,19 @@ public class OutputHandler {
 			this.groupCount = groupCount;
 		}
 		
-		public boolean match(String line) {
+		public boolean match(String line) throws TestingException {
 			matcher = enterPattern.matcher(line);
-			boolean matches = matcher.matches() && (groupCount == -1 || matcher.groupCount() == groupCount);
-			if (!matches) {
+			boolean groupsCountOk = groupCount == -1 || matcher.groupCount() == groupCount;
+			if (!groupsCountOk) {
+				generateInternalError(
+					MessageFormat.format(
+						"State with pattern \"{0}\" should has {1} groups but has {2}.",
+						enterPattern.pattern(), matcher.groupCount(), groupCount 
+					)
+				);
+			}
+			boolean matches = matcher.matches();
+			if (!matches || !groupsCountOk) {
 				// Do not keep the reference - it will be unnecessary anyway
 				matcher = null;
 			}
@@ -49,16 +58,27 @@ public class OutputHandler {
 		public void onEnter(State previousState) throws TestingException {}
 
 		public void onExit(State nextState) {}
+		
+		protected String getTestSuiteName(String name, String typeParameter) {
+			return (typeParameter != null) ? MessageFormat.format("{0}({1})", name, typeParameter.trim()) : name; //$NON-NLS-1$
+		}
 	}
 	
 	
 	class TestSuiteStart extends State {
+		private String typeParameter;
+		
 		TestSuiteStart(String enterRegex, int groupCount) {
 			super(enterRegex, groupCount);
 		}
 
 		public void onEnter(State previousState) {
-			modelUpdater.enterTestSuite(group(1));
+			typeParameter = group(3);
+			modelUpdater.enterTestSuite(getTestSuiteName(group(1), typeParameter));
+		}
+		
+		public String getTypeParameter() {
+			return typeParameter;
 		}
 	}
 	
@@ -68,16 +88,18 @@ public class OutputHandler {
 		}
 
 		public void onEnter(State previousState) throws TestingException {
+			String testCaseName = group(2);
 			String lastTestSuiteName = modelUpdater.currentTestSuite().getName();
-			if (!lastTestSuiteName.equals(group(1))) {
+			String currTestSuiteName = getTestSuiteName(group(1), stateTestSuiteStart.getTypeParameter());
+			if (!lastTestSuiteName.equals(currTestSuiteName)) {
 				generateInternalError(
 					MessageFormat.format(
 						"A test case \"{0}\" belongs to test suite \"{1}\", but the last started suite is \"{2}\".",
-						group(2), group(1), lastTestSuiteName
+						testCaseName, currTestSuiteName, lastTestSuiteName
 					)
 				);
 			}
-			modelUpdater.enterTestCase(group(2));
+			modelUpdater.enterTestCase(testCaseName);
 		}
 	}
 	
@@ -110,14 +132,14 @@ public class OutputHandler {
 			} else if (fileNameIfLineAbsent != null) {
 				if (lineNumberCommon == null && lineNumberVS == null) {
 					messageFileName = fileNameIfLineAbsent;
-					messageLineNumber = 1;
+					messageLineNumber = DEFAULT_LOCATION_LINE;
 				} else {
 					generateInternalError("Unknown location format.");
 				}
 			}
 			// Check special case when file is not known - reset location
 			if (messageFileName.equals("unknown file")) { //$NON-NLS-1$
-				messageFileName = null;
+				messageFileName = DEFAULT_LOCATION_FILE;
 			}
 			// NOTE: For Visual Studio style there is also first part of the message at this line
 			messagePart = group(8);
@@ -172,6 +194,25 @@ public class OutputHandler {
 		}
 	}
 	
+	class TestTrace extends ErrorMessageLocation {
+		
+		TestTrace(String enterRegex, int groupCount) {
+			super(enterRegex, groupCount);
+		}
+
+		public void onEnter(State previousState) throws TestingException {
+			super.onEnter(previousState);
+			modelUpdater.addTestMessage(
+				getMessageFileName(),
+				getMessageLineNumber(),
+				ITestMessage.Level.Info,
+				getMessagePart()
+			);
+		}
+
+		
+	}
+	
 	class TestCaseEnd extends State {
 
 		TestCaseEnd(String enterRegex, int groupCount) {
@@ -180,11 +221,14 @@ public class OutputHandler {
 		
 		public void onEnter(State previousState) throws TestingException {
 			String lastTestSuiteName = modelUpdater.currentTestSuite().getName();
-			if (!lastTestSuiteName.equals(group(2))) {
+			String explicitTypeParameter = group(5); 
+			String typeParameter = explicitTypeParameter != null ? explicitTypeParameter : stateTestSuiteStart.getTypeParameter(); 
+			String currTestSuiteName = getTestSuiteName(group(2), typeParameter);
+			if (!lastTestSuiteName.equals(currTestSuiteName)) {
 				generateInternalError(
 					MessageFormat.format(
 						"A test case \"{0}\" belongs to test suite \"{1}\", but the last started suite is \"{2}\".",
-						group(2), group(1), lastTestSuiteName
+						group(2), currTestSuiteName, lastTestSuiteName
 					)
 				);					
 			}
@@ -206,7 +250,17 @@ public class OutputHandler {
 			} else {
 				generateInternalError(MessageFormat.format("Test status value \"{0}\" is unknown.", testStatusStr));
 			}
-			modelUpdater.setTestingTime(Integer.parseInt(group(5)));
+			String getParamValue = group(7);
+			if (getParamValue != null) {
+				modelUpdater.addTestMessage(
+						DEFAULT_LOCATION_FILE,
+						DEFAULT_LOCATION_LINE,
+						ITestMessage.Level.Info,
+						MessageFormat.format("Instantiated with GetParam() = \"{0}\"", getParamValue)
+					);
+				
+			}
+			modelUpdater.setTestingTime(Integer.parseInt(group(8)));
 			modelUpdater.setTestStatus(testStatus);
 			modelUpdater.exitTestCase();
 		}
@@ -220,11 +274,12 @@ public class OutputHandler {
 		
 		public void onEnter(State previousState) throws TestingException {
 			String lastTestSuiteName = modelUpdater.currentTestSuite().getName();
-			if (!lastTestSuiteName.equals(group(1))) {
+			String currTestSuiteName = getTestSuiteName(group(1), stateTestSuiteStart.getTypeParameter());
+			if (!lastTestSuiteName.equals(currTestSuiteName)) {
 				generateInternalError(
 					MessageFormat.format(
 						"End of test suite \"{0}\" is not expected, because the last started suite is \"{1}\".",
-						group(1), lastTestSuiteName
+						currTestSuiteName, lastTestSuiteName
 					)
 				);					
 			}
@@ -237,6 +292,7 @@ public class OutputHandler {
 
 	// Common regular expression parts
 	static private String regexTestSuiteName = "([^,]+)"; //$NON-NLS-1$
+	static private String regexParameterInstantiation = "(\\s*,\\s+where\\s+TypeParam\\s*=([^,(]+))?"; //$NON-NLS-1$
 	static private String regexTestName = regexTestSuiteName+"\\.([^,]+)"; //$NON-NLS-1$
 	static private String regexTestCount = "\\d+\\s+tests?"; //$NON-NLS-1$
 	static private String regexTestTime = "(\\d+)\\s+ms"; //$NON-NLS-1$
@@ -264,29 +320,38 @@ public class OutputHandler {
 	// All available states
 	private State stateInitial = new State(""); //$NON-NLS-1$
 	private State stateInitialized = new State(".*Global test environment set-up.*"); //$NON-NLS-1$
-	private State stateTestSuiteStart = new TestSuiteStart("\\[-*\\]\\s+"+regexTestCount+"\\s+from\\s+"+regexTestSuiteName+"(|,.+)", 2); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+	private TestSuiteStart stateTestSuiteStart = new TestSuiteStart("\\[-*\\]\\s+"+regexTestCount+"\\s+from\\s+"+regexTestSuiteName+regexParameterInstantiation, 3); //$NON-NLS-1$ //$NON-NLS-2$
 	private State stateTestCaseStart = new TestCaseStart("\\[\\s*RUN\\s*\\]\\s+"+regexTestName, 2); //$NON-NLS-1$
 	private ErrorMessageLocation stateErrorMessageLocation = new ErrorMessageLocation(regexLocation+"\\s+(Failure|error: (.*))", 8); //$NON-NLS-1$
 	private State stateErrorMessage = new ErrorMessage("(.*)", 1); //$NON-NLS-1$
-	private State stateTestCaseEnd = new TestCaseEnd("\\[\\s*("+testStatusOk+"|"+testStatusFailed+")\\s*\\]\\s+"+regexTestName+"(|,.+)\\s+\\("+regexTestTime+"\\)", 5); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
+	private State stateTestTraceStart = new State(".*Google Test trace.*"); //$NON-NLS-1$
+	// NOTE: Use 8 groups instead of 7 cause we need to be consistent with ErrorMessageLocation (as we subclass it)
+	private State stateTestTrace = new TestTrace(regexLocation+"\\s+((.*))", 8); //$NON-NLS-1$
+	private State stateTestCaseEnd = new TestCaseEnd("\\[\\s*("+testStatusOk+"|"+testStatusFailed+")\\s*\\]\\s+"+regexTestName+regexParameterInstantiation+"(\\s*,\\s+where\\s+GetParam\\s*\\(\\s*\\)\\s*=\\s*\"([^\"]+)\")?\\s+\\("+regexTestTime+"\\)", 8); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
 	private State stateTestSuiteEnd = new TestSuiteEnd("\\[-*\\]\\s+"+regexTestCount+"\\s+from\\s+"+regexTestSuiteName+"\\s+\\("+regexTestTime+"\\s+total\\)", 2); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 	private State stateFinal = new State(".*Global test environment tear-down.*"); //$NON-NLS-1$
+	// NOTE: This state is a special workaround for empty test modules (they haven't got global test environment set-up/tear-down). They should be always passed.
+	private State stateEmptyTestModuleFinal = new State(".*\\[\\s*PASSED\\s*\\]\\s+0\\s+tests.*"); //$NON-NLS-1$
 	
 	private State currentState;
 	private Map<State, State[] > transitions = new HashMap<State, State[]>();
 	{
 		// NOTE: Next states order is important!
-		transitions.put(from(stateInitial), to(stateInitialized));
+		transitions.put(from(stateInitial), to(stateInitialized, stateEmptyTestModuleFinal));
 		transitions.put(from(stateInitialized), to(stateTestSuiteStart));
 		transitions.put(from(stateTestSuiteStart), to(stateTestCaseStart));
 		transitions.put(from(stateTestCaseStart), to(stateTestCaseEnd, stateErrorMessageLocation));
-		transitions.put(from(stateErrorMessageLocation), to(stateErrorMessage));
-		transitions.put(from(stateErrorMessage), to(stateTestCaseEnd, stateErrorMessageLocation, stateErrorMessage));
+		transitions.put(from(stateErrorMessageLocation), to(stateTestTraceStart, stateTestCaseEnd, stateErrorMessageLocation, stateErrorMessage));
+		transitions.put(from(stateErrorMessage), to(stateTestTraceStart, stateTestCaseEnd, stateErrorMessageLocation, stateErrorMessage));
+		transitions.put(from(stateTestTraceStart), to(stateTestTrace));
+		transitions.put(from(stateTestTrace), to(stateTestCaseEnd, stateErrorMessageLocation, stateTestTrace));
 		transitions.put(from(stateTestCaseEnd), to(stateTestCaseStart, stateTestSuiteEnd));
 		transitions.put(from(stateTestSuiteEnd), to(stateTestSuiteStart, stateFinal));
-		checkStateMachine();
 	}
 	
+	private static final String DEFAULT_LOCATION_FILE = null;
+	private static final int DEFAULT_LOCATION_LINE = 1;
+    
 	
 	OutputHandler(ITestModelUpdater modelUpdater) {
 		this.modelUpdater = modelUpdater;
@@ -297,6 +362,7 @@ public class OutputHandler {
 		InputStreamReader streamReader = new InputStreamReader(inputStream);
 		BufferedReader reader = new BufferedReader(streamReader);
         String line;
+        boolean finalizedProperly = false;
 
         // Initialize internal state
 		currentState = stateInitial;
@@ -305,6 +371,7 @@ public class OutputHandler {
         	State[] possibleNextStates = transitions.get(currentState);
         	if (possibleNextStates == null) {
         		// Final state, stop running
+        		finalizedProperly = true;
         		break;
         	}
         	for (State nextState : possibleNextStates) {
@@ -321,6 +388,13 @@ public class OutputHandler {
         	//       (e.g. some framework notes or warnings may be uncovered etc.), so we just skip unmatched 
         	//       lines without an error
         }
+        // Check whether the last line leads to the final state
+        if (transitions.get(currentState) == null) {
+    		finalizedProperly = true;
+        }
+        if (!finalizedProperly) {
+        	generateInternalError("Unexpected test module output.");
+        }
 	}
 	
 	private void generateInternalError(String additionalInfo) throws TestingException
@@ -328,15 +402,6 @@ public class OutputHandler {
 		throw new TestingException("Unknown error during parsing Google Test module output: "+additionalInfo);
 	}
 
-	private void checkStateMachine() {
-		// Check if only final state has no transitions
-		for (State state : transitions.keySet()) {
-			if (transitions.get(state).length == 0 && state != stateFinal) {
-				Activator.logErrorMessage("Google Test output parsing state machine check was failed: only final state should have no transitions");
-			}
-		}
-	}
-	
 	private State from(State fromState) {
 		return fromState;
 	}
